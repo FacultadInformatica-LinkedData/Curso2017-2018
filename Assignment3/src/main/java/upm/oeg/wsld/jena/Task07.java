a5e4491bc5122944bbe6251c46f7940e270ea3f9
Idafen Santana Pérez <isantana@fi.upm.es>
2017-10-17 10:43:26 +0200

    Update Task07.java

--- Assignment3/src/main/java/upm/oeg/wsld/jena/Task07.java
+++ Assignment3/src/main/java/upm/oeg/wsld/jena/Task07.java
@@ -25,7 +25,7 @@ public class Task07
 		String filename = "resources/example6.rdf";
 		
 		// Create an empty model
-		OntModel model = ModelFactory.createOntologyModel(OntModelSpec.RDFS_MEM_RDFS_INF);
+		OntModel model = ModelFactory.createOntologyModel(OntModelSpec.RDFS_MEM);
 		
 		// Use the FileManager to find the input file
 		InputStream in = FileManager.get().open(filename);
