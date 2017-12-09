516e26af02b3147fe06857132f2a956849ce9c44
LuisFernando100 <luis.gil.arroyo@alumnos.upm.es>
2017-12-09 23:23:48 +0100

    Loading icon and task for connecting added

--- HandsOn/Group11/application/source code/src/application/Main.java
+++ HandsOn/Group11/application/source code/src/application/Main.java
@@ -1,7 +1,8 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.event.ActionEvent;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -9,13 +10,13 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -27,10 +28,12 @@ public class Main extends Application {
 		try {
 			BorderPane root = new BorderPane();
 			root.setPadding(new Insets(100, 0, 0, 0));
+			
 			//Welcome text
 			Text text = new Text();
 			text.setText("Welcome to Madrid Events");
 			text.setFont(Font.font("Arial", 30));
+			
 			//Error text
 			Text textData = new Text();
 			textData.setText("Couldn't connect to data");
@@ -42,30 +45,58 @@ public class Main extends Application {
 			vbox.setSpacing(100);
 			vbox.setAlignment(Pos.CENTER);
 			root.setTop(vbox);
+			
 			//Start button
 			Button startButton = new Button("Start");
 			startButton.setPrefSize(100, 100);
-			startButton.setOnAction(new EventHandler<ActionEvent>() {
-	            @Override
-	            public void handle(ActionEvent event) {
-	            	if(connectToData())
-	            		primaryStage.close();
-	            	else{
-	            		textData.setVisible(true);
-	            		startButton.setText("Try again");
-	            	}
-	            }
-	        });
+		    startButton.setOnAction(e -> {
+		    	textData.setVisible(false);
+				Task<Boolean> connectingTask = new Task<Boolean>() {
+
+			        @Override
+			        protected Boolean call() throws Exception {
+			        	return connectToData();
+			        }
+			    };
+			    connectingTask.addEventHandler(WorkerStateEvent.WORKER_STATE_SUCCEEDED,
+			    	    new EventHandler<WorkerStateEvent>() {
+			        @Override
+			        public void handle(WorkerStateEvent t) {
+			            if(connectingTask.getValue()){
+			            	primaryStage.close();
+			            }
+			            else{
+			            	textData.setVisible(true);
+		            		startButton.setText("Try again");
+		            		startButton.setVisible(true);
+		            		root.setCenter(startButton);
+			            }
+			        }
+			    });
+		    	startButton.setVisible(false);
+            	Image loading = new Image("resources/loading.gif");
+            	ImageView loadingView = new ImageView(loading);
+            	loadingView.setPreserveRatio(true);
+            	loadingView.setFitHeight(150);;
+            	root.setCenter(loadingView);
+		        new Thread(connectingTask).start();
+		    });
+		    
 			root.setCenter(startButton);
+			
 			//Main scene
 			Scene scene = new Scene(root,800,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
 			//Title
 			primaryStage.setTitle("Madrid Events");
+			
 			//Background
 			BackgroundImage bck = new BackgroundImage(new Image("resources/background.jpg"), BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,new BackgroundSize(100, 100, true, true, true, true));
 			root.setBackground(new Background(bck));
+			
 			//Deployment
+			primaryStage.setResizable(false);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -79,6 +110,12 @@ public class Main extends Application {
 	
 	private boolean connectToData() {
 		//TODO Connect to data on AWS or local server Virtuoso
+		
+		//DEBUG
+		for(int i = 0; i<1000000; i++){
+			System.out.println(i);
+		}
+		
 		return false;
 	}
 }
