ec5a75daee187949ff22a21acf9a7de4f55c02a2
MiguelERuiz <meruiz77@gmail.com>
2017-12-10 11:55:11 +0100

    BusController edited and Application added

--- HandsOn/Group12/src/main/java/BusApp/BusController.java
+++ HandsOn/Group12/src/main/java/BusApp/BusController.java
@@ -1,5 +1,14 @@
 package BusApp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class BusController {
+
+    @RequestMapping("/")
+    public ModelAndView index(){
+        return new ModelAndView("index");
+    }
 }
