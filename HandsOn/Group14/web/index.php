7373a0d399d94236d0df8233d5842e3d9accf127
Víctor Martín Fernández <victor.mf@outlook.es>
2017-12-15 17:18:12 +0100

    App index moved and improved

--- HandsOn/Group14/web/index.php
+++ HandsOn/Group14/web/index.php
@@ -1,4 +1,18 @@
 <html>
+<head>
+  <meta charset="UTF-8">
+  <title>Group 14</title>
+  <base href="/">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+</head>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="/web">Turismo en Madrid</a>
+  </div>
+</nav>
   <body>
  
   <?php
@@ -16,20 +30,15 @@
   }
  
   $query = '
-  PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
-  PREFIX owl: <http://www.w3.org/2002/07/owl#>
-  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
   PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-  PREFIX dc: <http://purl.org/dc/elements/1.1/>
-  PREFIX : <http://dbpedia.org/resource/>
-  PREFIX dbpedia2: <http://dbpedia.org/property/>
-  PREFIX dbpedia: <http://dbpedia.org/>
-  PREFIX dbpprop: <http://dbpedia.org/property/>
-  PREFIX ns: <http://www.wsdlfiupm.es/resources/group14/class/>
+  PREFIX nsclass: <http://www.wsdlfiupm.es/resources/group14/class/>
+  PREFIX nsprop: <http://www.wsdlfiupm.es/resources/group14/properties/>
 
-  SELECT ?instances WHERE {?instances a ns:Monuments_Museums_Organizations.}';
+  SELECT ?instances ?image ?label WHERE {?instances a nsclass:Monuments_Museums_Organizations .
+  ?instances nsprop:image ?image .
+  ?instances rdfs:label ?label .}';
   
   /* execute the query */
   $rows = $store->query($query, 'rows'); 
@@ -38,22 +47,19 @@
        echo "Query errors" ;
        print_r($errs);
     }
- 
-    /* display the results in an HTML table */
-    echo "<table border='1'>
-    <thead>
-        <th>#</th>
-        <th>instances</th>
-    </thead>";
 
     /* loop for each returned row */
-	$id = 0;
+	echo '<div style="margin-left: 4%; display: grid; grid-template-columns: 0.5fr repeat(2, 0.5fr) 0.5fr; grid-template-rows: 1fr 1fr;">';
     foreach( $rows as $row ) { 
-		print "<tr><td>".++$id. "</td>
-		<td><a href='". $row['instances'] . "'>". $row['instances'] . "</td>";
-    }
-    echo "</table>" 
-
-  ?>
+	?>	
+	<div class="card bg-blue" style="cursor:pointer; width: 20em; margin-top: 6%" onclick="location.href='web/info.php?label=<?php echo $row['label']?>'">
+	  <img class="card-img-top" src="<?php echo $row['image']?>" alt="Imagen monumento" style='width: auto; max-height: 15em; align-self: center;'>
+	  <div class="card-body">
+		<h4 class="card-title"><?php echo $row['label']?></h4>
+		<a href="web/info.php?label=<?php echo $row['label']?>" class="btn btn-primary">Mas información</a>
+	  </div>
+	</div>
+	<?php }?>
+	</div>
   </body>
 </html>
